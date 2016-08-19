cb45fb68f6eefc30f8b35ff459643118cae99d32
Aleksander Drenik <adrenik@ipp.mpg.de>
2016-08-19 14:57:53 +0200

    added __init__.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='intervals2D',
     version='1.0',
-    py_modules=['intervals2D'],
+    py_modules=['intervals2D', '__init__'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
